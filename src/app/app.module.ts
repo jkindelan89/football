ba0c0d8da2dc8a5e05395217aee03949b75a7461
Kindelan <jose.kindelan@peoplewalking.com>
2021-10-27 11:53:23 -0400

    configuracion del template a usar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MenubarModule} from 'primeng/menubar';
+import {CardModule} from 'primeng/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
+    MenubarModule,
+    CardModule,
     AppRoutingModule
   ],
   providers: [],
